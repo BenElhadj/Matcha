8dfc368e4676f1a1961270b1035acc222d15dd34
BEN ELHADJ Hamdi <42bhamdi@gmail.com>
2023-07-25 04:41:37 +0200

    back-to-top ti fix in footer

--- frontend/src/components/FooterView.vue
+++ frontend/src/components/FooterView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="footer">
-    <div class="divider first-divider"></div>
+    <div class="divider first-divider">
+      <img class="back-to-top" src="@/assets/Footer/back-to-top.png" alt="Back to top" @click="moveUp" :style="{ opacity: backToTopOpacity }" :disabled="isTop"/>
+    </div>
     <div class="full-container">
       <div class="top-container">
         <div class="contact-container">
@@ -12,7 +14,20 @@
           </div>
         </div>
         <div class="logo-container">
-          <img src="@/assets/Footer/matcha.png" alt="Logo">
+          <div>
+            <img src="@/assets/Footer/matcha.png" alt="Logo">
+          </div>
+          <div class="bottom-container">
+            <q-item>
+              <p>
+                <a href="https://profile.intra.42.fr/users/bhamdi">Bhamdi</a>
+                &amp;
+                <a href="https://profile.intra.42.fr/users/mbouzaie">Mbouzae</a>
+                Vous présentent
+                <a href="https://projects.intra.42.fr/projects/42cursus-matcha">MATCHA © 2023</a>
+              </p>
+            </q-item>
+          </div>
         </div>
         <div class="follow-container">
           <div class="follow-text-container">
@@ -35,17 +50,6 @@
         </div>
       </div>
       <div class="divider"></div>
-      <div class="bottom-container">
-        <q-item>
-          <p>
-            <a href="https://profile.intra.42.fr/users/bhamdi">Bhamdi</a>
-            &amp;
-            <a href="https://profile.intra.42.fr/users/mbouzaie">Mbouzae</a><br />Vous présentent<br />
-            <a href="https://projects.intra.42.fr/projects/42cursus-matcha">MATCHA © 2023</a>
-          </p>
-        </q-item>
-      </div>
-      <img class="back-to-top" src="@/assets/Footer/back-to-top.png" alt="Back to top" @click="moveUp"/>
     </div>
   </div>
 </template>
@@ -53,12 +57,28 @@
 <script>
 export default {
   name: 'FooterView',
-  mounted () {
+  data() {
+    return {
+      scrollPosition: null,
+      backToTopOpacity: 0,
+      isTop: true,
+    };
   },
-
+  mounted() {
+    window.addEventListener('scroll', this.updateScroll);
+    this.updateScroll();
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.updateScroll);
+  },
   methods: {
-    moveUp () {
-      window.scroll({ top: 0, left: 0, behavior: 'smooth' })
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+      this.backToTopOpacity = Math.min(this.scrollPosition / 1000, 1);
+      this.isTop = this.scrollPosition === 0;
+    },
+    moveUp() {
+      window.scroll({ top: 0, behavior: 'smooth' })
     }
   }
 }
@@ -69,7 +89,9 @@ export default {
   font-weight: bold;
   align-items: center;
   text-align: center;
-  font-size: 16px;
+  /* font-size: 16px; */
+  font-size: clamp(12px, 2em, 18px);
+  max-height: fit-content;
 }
 
 .footer {
@@ -89,9 +111,12 @@ export default {
   z-index: 1;
 }
 .full-container {
-  width: 95%;
+  position: relative;
+  width: 95vw;
   min-width: 670px;
   margin: auto;
+  max-height: 30vh;
+  min-height: 10vh;
 }
 
 .top-container {
@@ -115,19 +140,23 @@ export default {
 }
 
 .links-contact-container img {
-  height: 37px;
+  max-height: 5vh;
+  min-height: 3vh;
   margin-right: 50px;
   border-right: 50px;
 }
 
 .logo-container {
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
   padding: 0 20px;
 }
 
 .logo-container img {
-  max-width: 100%;
+  max-height: 11vh;
+  min-height: 9vh;
   height: auto;
 }
 
@@ -135,11 +164,14 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  font-size: 18px;
+  /* font-size: 18px; */
+  font-size: clamp(14px, 2em, 20px);
   margin-left: auto;
 }
 
 .links-follow-container img {
+  max-height: 5vh;
+  min-height: 3vh;
   height: 40px;
 }
 
@@ -155,7 +187,8 @@ export default {
 
 .bottom-container {
   display: flex;
-  font-size: 26px;
+  /* font-size: 26px; */
+  font-size: clamp(22px, 2em, 28px);
   text-align: center;
   margin-bottom: 17px;
 }
@@ -169,10 +202,15 @@ export default {
 }
 
 .back-to-top {
-  position: fixed;
+  position: absolute;
+  max-height: 15vh;
+  min-height: 13vh;
   cursor: pointer;
-  right: 45px;
-  bottom: 30px;
-  height: 130px;
+  right: 35px;
+  top: -100px;
 }
+.back-to-top:disabled {
+  pointer-events: none;
+}
+
 </style>
