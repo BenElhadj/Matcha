a28123dfa5fb71ec4a3bb0a44e4f59192faa1cb5
Hamdi Ben Elhadj <bhamdi@student.42.fr>
2025-10-23 00:50:11 +0200

    fix using SendGrid instead of gmail

--- backend/src/config/smtp.js
+++ backend/src/config/smtp.js
@@ -1,12 +1,9 @@
 const smtp = {
-	host: process.env.MAIL_SMTP,
-	port: process.env.MAIL_PORT,
-	secure: false,
-	auth: {
-			service: 'SendGrid',
-			user: 'apikey',
-			pass: process.env.SENDGRID_API_KEY
-	}
+  service: 'SendGrid',
+  auth: {
+    user: 'apikey',
+    pass: process.env.SENDGRID_API_KEY
+  }
 }
 
 module.exports = smtp
