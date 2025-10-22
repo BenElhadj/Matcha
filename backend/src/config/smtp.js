93cd4b636a707d9f67864fe5570230b46e6980ec
Hamdi Ben Elhadj <bhamdi@student.42.fr>
2025-10-22 22:51:27 +0200

    fix smtp

--- backend/src/config/smtp.js
+++ backend/src/config/smtp.js
@@ -1,11 +1,12 @@
 const smtp = {
-  host: process.env.MAIL_SMTP,
-  port: process.env.MAIL_PORT,
-  secure: false,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  }
+	host: process.env.MAIL_SMTP,
+	port: process.env.MAIL_PORT,
+	secure: false,
+	auth: {
+		type: 'OAuth2',
+		user: process.env.MAIL_USER,
+		pass: process.env.MAIL_PASS
+	}
 }
 
 module.exports = smtp
